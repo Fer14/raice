74a5aab0a772a846fd3bcc1bbab32783a92a4bac
Fer14 <fcsancho14@gmail.com>
2024-09-08 14:42:14 +0200

    update launch race

--- launch_race.py
+++ launch_race.py
@@ -1,5 +1,7 @@
 from race import Race
 from neat_.neat_car import NeatCar
+from policy_gradient.pg_car import PGCar
+from qlearning.q_car import QCar
 
 
 def main():
@@ -11,7 +13,13 @@ def main():
     car = NeatCar(position=race.start)
     car.load_net()
 
-    race.race(cars=[car])
+    car2 = PGCar(position=race.start)
+    car2.load_policy()
+
+    car3 = QCar(position=race.start)
+    car3.load()
+
+    race.race(cars=[car, car2, car3])
 
 
 if __name__ == "__main__":
